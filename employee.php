610076ec75509b8f3a39e579f542abdfc6b08771
Kosjak76 <kosjak76@gmail.com>
2022-10-06 18:27:22 +0300

    Fix

--- employee.php
+++ employee.php
@@ -9,11 +9,11 @@ class Employee
 $employee1 = new Employee();
 $employee1->name = 'John';
 $employee1->age = 25;
-$employee1->salary = '1000';
+$employee1->salary = 1000;
 $employee2 = new Employee();
 $employee2->name = 'Erik';
 $employee2->age = 26;
-$employee2->salary = '2000';
+$employee2->salary = 2000;
 
 print  "Both employees salary: " . $employee1->salary + $employee2->salary . "<br />";
 print  "Both employees age: " . $employee1->age + $employee2->age;
